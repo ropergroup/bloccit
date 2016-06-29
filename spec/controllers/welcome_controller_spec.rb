a79c23cc09c847a40ff8aaf8462f801f93dbd021
Chris Roper <chris@roper.xyz>
2016-06-29 14:49:09 -0600

    CP26 Assignment

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -13,5 +13,11 @@ RSpec.describe WelcomeController, type: :controller do
        get :about
        expect(response).to render_template("about")
      end
-   end 
-end
+   end
+   describe "GET faq" do
+     it "renders the faq template" do
+       get :faq
+       expect(response).to render_template("faq")
+     end
+   end
+ end
