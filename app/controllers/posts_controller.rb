934f03f117ac2c54ead30d760b4da2db75aa3c75
Chris Roper <chris@roper.xyz>
2016-07-09 16:16:04 -0600

    CP31A

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,14 @@ class PostsController < ApplicationController
   def index
 
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
+
+
 
   def show
   end
