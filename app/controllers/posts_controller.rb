cf37550ba93531cd5953c06214f9af4cacd67ade
Chris Roper <chris@roper.xyz>
2016-07-11 10:30:07 -0600

    CP32

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,30 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+
+       @post = Post.new
+       @post.title = params[:post][:title]
+       @post.body = params[:post][:body]
+
+
+       if @post.save
+
+         flash[:notice] = "Post was saved successfully."
+         redirect_to @post
+       else
+
+         flash.now[:alert] = "There was an error saving the post. Please try again."
+         render :new
+       end
+     end
 
   def edit
   end
