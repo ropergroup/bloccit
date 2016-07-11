ae4c9b6d2fd6ac407320173eb1a7d67b28270237
Chris Roper <chris@roper.xyz>
2016-07-11 11:00:36 -0600

    CP32A up to step 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/edit'
+  resources :advertisements
 
   resources :posts
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706175647) do
+ActiveRecord::Schema.define(version: 20160711025028) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
