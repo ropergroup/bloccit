c27820b99fadfef186427ee9bb88d8825002fc34
Chris Roper <chris@roper.xyz>
2016-07-12 16:28:59 -0600

    CP33

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,22 +14,50 @@ class PostsController < ApplicationController
 
   def create
 
-       @post = Post.new
-       @post.title = params[:post][:title]
-       @post.body = params[:post][:body]
+     @post = Post.new
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
 
 
-       if @post.save
+     if @post.save
 
-         flash[:notice] = "Post was saved successfully."
-         redirect_to @post
-       else
+       flash[:notice] = "Post was saved successfully."
+       redirect_to @post
+     else
 
-         flash.now[:alert] = "There was an error saving the post. Please try again."
-         render :new
-       end
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :new
      end
+   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+     @post = Post.find(params[:id])
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+
+     if @post.save
+       flash[:notice] = "Post was updated successfully."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+   end
+
+   def destroy
+      @post = Post.find(params[:id])
+
+
+      if @post.destroy
+        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+        redirect_to posts_path
+      else
+        flash.now[:alert] = "There was an error deleting the post."
+        render :show
+      end
+    end
 end
