7233d31f6cc20bbffff673946e41050a3d12f90b
Chris Roper <chris@roper.xyz>
2016-07-13 15:52:22 -0600

    CP34

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,11 @@
 class PostsController < ApplicationController
-  def index
-
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -17,12 +14,14 @@ class PostsController < ApplicationController
      @post = Post.new
      @post.title = params[:post][:title]
      @post.body = params[:post][:body]
+     @topic = Topic.find(params[:topic_id])
 
+     @post.topic = @topic
 
      if @post.save
 
        flash[:notice] = "Post was saved successfully."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
 
        flash.now[:alert] = "There was an error saving the post. Please try again."
@@ -41,7 +40,7 @@ class PostsController < ApplicationController
 
      if @post.save
        flash[:notice] = "Post was updated successfully."
-       redirect_to @post
+       redirect_to [@post.topic, @post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -54,7 +53,7 @@ class PostsController < ApplicationController
 
       if @post.destroy
         flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-        redirect_to posts_path
+        redirect_to @post.topic
       else
         flash.now[:alert] = "There was an error deleting the post."
         render :show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :topics
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+   end
 
   get 'about' => 'welcome#about'
 
