eeebbceb3a741f8a9b0b04dee031835c83385681
Chris Roper <chris@roper.xyz>
2016-07-15 11:01:12 -0600

    CP35A

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 end
