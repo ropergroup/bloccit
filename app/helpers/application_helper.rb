f7f37b6aefe6ae316d5a362041a65ed7c25131fa
Chris Roper <chris@roper.xyz>
2016-07-17 11:01:59 -0600

    CP38

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,5 @@ module ApplicationHelper
 
      content_tag :div, capture(&block), class: css_class
    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
    resources :users, only: [:new, :create]
 
+   resources :sessions, only: [:new, :create, :destroy]
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
