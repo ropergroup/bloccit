b761811e86f98b46c894d078fa64c5279e773065
Chris Roper <chris@roper.xyz>
2016-07-17 19:54:25 -0600

    CP39A

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> {order('title DESC')}
+  scope :ordered_by_reverse_created_at,-> {order('title ASC')}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
