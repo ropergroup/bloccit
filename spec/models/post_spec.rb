bd57a33e0bf9058bf7525d1f56185fe787d1d44e
Chris Roper <chris@roper.xyz>
2016-07-22 17:01:14 -0600

    CP44 Updated

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Post, type: :model do
    it { is_expected.to have_many(:votes) }
    it { is_expected.to belong_to(:topic) }
    it { is_expected.to belong_to(:user) }
+   it { is_expected.to have_many(:favorites) }
 
    it { is_expected.to validate_presence_of(:title) }
    it { is_expected.to validate_presence_of(:body) }
