51699e62d8627218f27ba075c7a68e00b5ed83ef
Chris Roper <chris@roper.xyz>
2016-07-24 08:31:09 -0600

    CP462

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,5 +4,8 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
-   scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -23,19 +23,31 @@ RSpec.describe Topic, type: :model do
    describe "scopes" do
       before do
 
-         @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+         @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: true)
          @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
       end
 
-      describe "visible_to(user)" do
-         it "returns all topics if the user is present" do
-            user = User.new
+         describe "visible_to(user)" do
+            it "returns all topics if the user is present" do
+               user = User.new
 
-            expect(Topic.visible_to(user)).to eq(Topic.all)
+               expect(Topic.visible_to(user)).to eq(Topic.all)
+            end
+
+            it "returns only public topics if user is nil" do
+               expect(Topic.visible_to(nil)).to eq([@public_topic])
+            end
          end
 
-         it "returns only public topics if user is nil" do
-            expect(Topic.visible_to(nil)).to eq([@public_topic])
+      describe "publicly_viewable" do
+         it "returns all public topics" do
+            expect(Topic.publicly_viewable).to eq([@public_topic])
+         end
+      end
+
+      describe "privately_viewable" do
+         it "returns all private topics" do
+            expect(Topic.privately_viewable).to eq([@private_topic])
          end
       end
    end
