207f2dd3c40a2112b56f9bfb2210a873ac07ad6f
Chris Roper <chris@roper.xyz>
2016-07-25 16:28:23 -0600

    CP49A Topic add

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,12 +5,12 @@ class Api::V1::PostsController < Api::V1::BaseController
 
    def index
       posts = Post.all
-      render json: posts, status: 200
+      render json: topics.to_json(include: :posts), status: 200
    end
 
    def show
       post = Post.find(params[:id])
-      render json: post, status: 200
+      render json: topics.to_json(include: :posts), status: 200
    end
 
    def update
